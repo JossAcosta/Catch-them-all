246e493530f648169dc978f8a17ee3fc784c1064
jossAcosta <josselineacostamtz@gmail.com>
2020-04-17 03:51:03 -0500

    Updates

---
+++ precache-manifest.d86921f0e701f62a01a3efc429058761.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6047fa857269e564e46f9a55615f9994",
+    "url": "/Catch-them-all/index.html"
+  },
+  {
+    "revision": "f4eceb6d4872ab92b8cc",
+    "url": "/Catch-them-all/static/css/main.0ea063dd.chunk.css"
+  },
+  {
+    "revision": "e032064dfb562e910971",
+    "url": "/Catch-them-all/static/js/2.8597ea3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Catch-them-all/static/js/2.8597ea3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4eceb6d4872ab92b8cc",
+    "url": "/Catch-them-all/static/js/main.7ec9f408.chunk.js"
+  },
+  {
+    "revision": "ad080fd736f7b1152c75",
+    "url": "/Catch-them-all/static/js/runtime-main.db04de38.js"
+  },
+  {
+    "revision": "980b9f32a3f482b7db688fa8df0488cd",
+    "url": "/Catch-them-all/static/media/Manrope-VariableFont_wght.980b9f32.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/Catch-them-all/static/media/Pokemon Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "33dbecea47ca2859427ae50c98b634ab",
+    "url": "/Catch-them-all/static/media/Spin.33dbecea.gif"
+  },
+  {
+    "revision": "6b4ffa55fdd7e087c6990685887572b4",
+    "url": "/Catch-them-all/static/media/pokeball.6b4ffa55.svg"
+  }
+]);
